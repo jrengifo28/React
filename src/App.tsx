b90c0695357ccc2898e7353517c3f7c6e9cc5e1d
Jhonathan Rengifo <jrengifo28@hotmail.com>
2023-06-20 00:22:54 -0500

    Creating a Custom Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { useReducer } from "react";
 import "./App.css";
+import AuthProvider from "./state-management/AuthProvider";
+import HomePage from "./state-management/HomePage";
+import NavBar from "./state-management/NavBar";
 import TasksContext from "./state-management/contexts/tasksContext";
 import taskReducer from "./state-management/reducers/taskReducer";
-import NavBar from "./state-management/NavBar";
-import HomePage from "./state-management/HomePage";
-import authReducer from "./state-management/reducers/authReducer";
-import AuthContext from "./state-management/contexts/authContext";
-import AuthProvider from "./state-management/AuthProvider";
 
 function App() {
   const [tasks, taskDispatch] = useReducer(taskReducer, []);
